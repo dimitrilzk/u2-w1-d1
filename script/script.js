218fe8d54d1ab3fbd53e7a7cd727afa395c56754
dimitrilzk <dimitrilazuka95@gmail.com>
2022-09-26 15:30:05 +0200

    fix

--- script/script.js
+++ script/script.js
@@ -3,3 +3,8 @@ document.getElementById("mostra").innerHTML = tempo;
 document.getElementById("ore").innerHTML = tempo.getHours();
 document.getElementById("minuti").innerHTML = tempo.getMinutes();
 document.getElementById("secondi").innerHTML = tempo.getSeconds();
+let tempoIta = new Date();
+document.getElementById("data-italiana").innerHTML = tempoIta.toLocaleString("it-IT");
+document.getElementById(
+  "solo-data"
+).innerHTML = `${tempoIta.getDay()}:${dateTime.getSeconds()}:${dateTime.getMilliseconds()}`;

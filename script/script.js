5dc1c9ce9203421d551a320d2e53c3f3a94e7dde
dimitrilzk <dimitrilazuka95@gmail.com>
2022-09-26 16:17:58 +0200

    fine

--- script/script.js
+++ script/script.js
@@ -3,10 +3,12 @@ document.getElementById("mostra").innerHTML = tempo;
 document.getElementById("ore").innerHTML = tempo.getHours();
 document.getElementById("minuti").innerHTML = tempo.getMinutes();
 document.getElementById("secondi").innerHTML = tempo.getSeconds();
+
 let tempoIta = new Date();
 document.getElementById("data-italiana").innerHTML = tempoIta.toLocaleString("it-IT");
 
 let data = new Date();
+
 let dataIta = data.toLocaleDateString("it-IT");
 document.getElementById("solo-data").innerHTML = dataIta;
 
